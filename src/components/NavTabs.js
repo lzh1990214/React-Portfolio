0a87e6990128e38d6fb73219879c0560e73e03e4
zhihao li <zhihaoli@zhihaos-MacBook-Pro.local>
2023-04-08 15:58:32 -0400

    modified About me page

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -4,49 +4,52 @@ import React from 'react';
 // We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <a
-          href="#home"
-          onClick={() => handlePageChange('Home')}
-          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-        >
-          Home
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-        >
-          About
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#blog"
-          onClick={() => handlePageChange('Blog')}
-          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
-        >
-          Blog
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
-          onClick={() => handlePageChange('Contact')}
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </a>
-      </li>
-    </ul>
+    <div class="container-fluid d-flex align-items-baseline justify-content-between mt-3">
+      <a class="navbar-brand" href="#">ZHIHAO LI</a>
+      <ul className="nav justify-content-end nav-pills">
+        <li className="nav-item">
+          <a
+            href="#about"
+            onClick={() => handlePageChange('About')}
+            // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+            className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+          >
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#blog"
+            onClick={() => handlePageChange('Portfolio')}
+            // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+            className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+          >
+            Portfolio
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#contact"
+            onClick={() => handlePageChange('Contact')}
+            // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+          >
+            Contact
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#resume"
+            onClick={() => handlePageChange('Resume')}
+            // This is a conditional (ternary) operator that checks to see if the current page is "Resume"
+            // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+            className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+          >
+            Resume
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
 
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,15 +2,19 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+    <div className="container-sm d-flex flex-column align-items-center mt-5 mw-75" >
+      <h1 className='mb-5'>About Me</h1>
+      <p className='w-75'>
+        Zhihao Li is a talented landscape architect with over seven years of experience designing and managing projects of various scales at AECOM in Washington D.C.. He holds a Master's degree in Landscape Architecture from Rhode Island School of Design, where he developed his skills in site analysis, artistic view, and creative problem-solving.
+      </p>
+      <p className='w-75'>
+        However, Zhihao's passion for technology led him to pursue a career transition into the UX field, and he earned an MPS in UX Design from Maryland Institute College of Art. To further enhance his technical expertise, he recently completed a Full-Stack Development Certificate from the George Washington University bootcamp, where he acquired skills such as HTML, CSS, JavaScript, MongoDB, Express, React and Node.js.
+      </p>
+      <p className='w-75'>
+        Zhihao's background in landscape architecture, education in UX design, and technical proficiency in full-stack development make him a versatile designer capable of tackling complex challenges from multiple perspectives. He believes that his unique combination of skills enables him to design innovative digital products that not only look eye-catching but also function to enhance the user experience.
+      </p>
+      <p className='w-75'>
+        Zhihao is a dedicated worker who is always eager to learn and grow. He enjoys keeping up with the latest trends in design and technology and applying his knowledge to his work. When not working, Zhihao loves to travel, take photographs, and learn new languages.
       </p>
     </div>
   );
