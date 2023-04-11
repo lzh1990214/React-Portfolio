679972273185de8367c3d20d80faccedd873c854
zhihao li <zhihaoli@zhihaos-MBP.fios-router.home>
2023-04-10 22:50:29 -0400

    modified json for deploy

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -26,22 +26,22 @@ function NavTabs({ currentPage, handlePageChange }) {
         >
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a href="#"
+              <a href="#About"
                 onClick={() => handlePageChange('About')}
                 // Check to see if the currentPage is `About`, and if so, the element uses the active link class from bootstrap. Otherwise, set the class to a normal nav-link
                 className={currentPage === 'About' ? 'nav-link active' : 'nav-link'} id={currentPage === 'About' ? 'about' : 'about-fade'}>About</a>
             </li>
             <li className="nav-item">
-              <a href="#"
+              <a href="#Portfolio"
                 onClick={() => handlePageChange('Portfolio')}
                 className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'} id={currentPage === 'Portfolio' ? 'portfolio' : 'portfolio-fade'}>Portfolio</a>
             </li>
             <li className="nav-item">
-              <a href="#" onClick={() => handlePageChange('Contact')}
+              <a href="#Contact" onClick={() => handlePageChange('Contact')}
                 className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'} id={currentPage === 'Contact' ? 'contact' : 'contact-fade'}>Contact</a>
             </li>
             <li className="nav-item">
-              <a href="#"
+              <a href="#Resume"
                 onClick={() => handlePageChange('Resume')}
                 // checks to see if the current page is "Resume".
                 // If it is, set the current page to 'nav-link-active', otherwise set it to 'nav-link'
